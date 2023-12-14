ae1e7cb62d8740af5fc8da02aefbe52c2af11dbd
vvcaw <eric.elflein@gmail.com>
2023-12-14 17:02:29 +0100

    Add colors & grid

---
+++ src/Colors.hs
@@ -0,0 +1,12 @@
+module Colors (backgroundColor, purple, textColor) where
+
+import Codec.Picture.Types
+
+backgroundColor :: PixelRGBA8
+backgroundColor = PixelRGBA8 0xfd 0xf9 0xf3 0xff
+
+purple :: PixelRGBA8
+purple = PixelRGBA8 0xab 0x9d 0xf2 0xff
+
+textColor :: PixelRGBA8
+textColor = PixelRGBA8 0x2c 0x29 0x2d 0xff
--- src/Main.hs
+++ src/Main.hs
@@ -1,29 +1,87 @@
 module Main (main, reanimateLive) where
 
 import Control.Lens
+import qualified Data.Text as T
 import Main.Utf8 (withUtf8)
 import Reanimate
 import Reanimate.Builtin.Documentation
 import Reanimate.Scene
+import Reanimate.Voice
+import Text.Printf
+import Colors
 
 main :: IO ()
 main =
   withUtf8 . reanimate $ sinExample
 
+sinExampleTranscript = fakeTranscript "As an example we will use the sin function\n"
+
 sinExample :: Animation
 sinExample = env $
   scene $ do
-    obj <- oNew equation
-    oShow obj
+    -- eq <- oNew equation
+    -- oShowWith eq oDraw
+    -- wait 1
+    -- oHideWith eq oFadeOut
 
-    oTweenS obj 1 moveTopLeftAndScaleDownSlightly
+    -- wait 1
+
+    newSpriteSVG_ grid
+    newSpriteSVG_ $ redDot (0.0, 0.0)
+    newSpriteSVG_ $ generateEvenlySpacedDotsOfSin 5
 
     wait 1
 
-moveTopLeftAndScaleDownSlightly t = do
-  oCenterX (Double -> f Double) (ObjectData a)
-  oScale %= \origin ->
-    fromToS origin 0.5 t
+generateEvenlySpacedDotsOfSin dotCount = mkGroup [redDot p | p <- helper (-8)]
+  where
+    helper xVal = if xVal < 16 then (xVal, ((*3.5) . sin) xVal):helper (xVal + stepSize) else []
+    stepSize = 16 / dotCount
+
+outlinedText :: Text -> SVG
+outlinedText txt =
+  mkGroup
+    [ center $
+        withStrokeColorPixel backgroundColor $
+          withStrokeWidth (defaultStrokeWidth * 4) $
+            withFillOpacity 0 $
+              latex txt,
+      withStrokeWidth (defaultStrokeWidth * 0.5) $ withStrokeColorPixel textColor $ center $ latex txt
+    ]
+
+redDot :: (Double, Double) -> SVG
+redDot (x, y) =
+  translate x y $ withFillColorPixel purple $ mkCircle 0.1
+
+redDotLabeled :: (Double, Double) -> SVG
+redDotLabeled (x, y) =
+  translate x y $
+    mkGroup
+      [ translate 0 (-0.5) $ scale 0.5 $ outlinedText $ T.pack $ printf "%.1f, %.1f" x y,
+        withFillColorPixel purple $ mkCircle 0.1
+      ]
+
+grid :: SVG
+grid =
+  mkGroup
+    [ withStrokeColorPixel textColor $ mkLine (- screenWidth, 0) (screenWidth, 0),
+      withStrokeColorPixel textColor $ mkLine (0, - screenHeight) (0, screenHeight),
+      withStrokeColorPixel textColor $
+        withStrokeWidth (defaultStrokeWidth * 0.5) $
+          mkGroup
+            [ mkGroup
+                [ translate
+                    0
+                    (i / screenHeight * screenHeight - screenHeight / 2 - screenHeight / 18)
+                    $ mkLine (- screenWidth, 0) (screenWidth, 0)
+                  | i <- [0 .. screenHeight]
+                ],
+              mkGroup
+                [ translate (i / screenWidth * screenWidth - screenWidth / 2) 0 $
+                    mkLine (0, - screenHeight) (0, screenHeight)
+                  | i <- [0 .. screenWidth]
+                ]
+            ]
+    ]
 
 equation :: SVG
 equation =
